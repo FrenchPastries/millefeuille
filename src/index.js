4c89f587a7a17997afbe5e221bb2253b20c00f5d
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-10-23 21:15:17 +0200

    :construction: Research perfect API

--- src/index.js
+++ src/index.js
@@ -13,6 +13,32 @@ const stringifyBody = handler => request => {
   return newValue
 }
 
-const server = Server.create(stringifyBody(handler))
+const allRoutes = routes({
+  '/users': get(handleUsers),
+  '/user/:id': {
+    '/': post(handleUser),
+    '/posts': post(handleUserPosts),
+    '/post/:id': post(handleUserPost)
+  },
+  '/products': get(handleProducts),
+  '/product/:id': get(handleProduct),
+  'not-found': handleNotFound
+})
+
+const allRoutes = routes(
+  get('/users', handleUsers),
+  context('/user/:id',
+    post('/', handleUser),
+    post('/posts', handleUserPosts),
+    post('/post/:id', handleUserPost)
+  ),
+  get('/products', handleProducts),
+  get('/product/:id', handleProduct),
+  notFound(handleNotFound)
+)
+
+const server = Server.create(
+  stringifyBody(allRoutes)
+)
 
 console.log(`Server Started on port: 8080`)
