6ad646ed50a8819cffd1320a3506edcc0dabe699
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-11-11 22:27:25 +0100

    :white_check_mark: Add tests for response utility

---
+++ test/response-test.js
@@ -0,0 +1,71 @@
+const should = require('chai').should() // eslint-disable-line
+const fetch = require('node-fetch')
+
+const util = require('../src/response')
+
+describe('Response utility', function() {
+  specify('response should be correct response', function() {
+    const res = util.response('Ok')
+    res.should.be.deep.equal({
+      statusCode: 200,
+      headers: {},
+      body: 'Ok'
+    })
+  })
+
+  specify('redirect should be correct redirect response', function() {
+    const res = util.redirect('https://frenchpastries.org')
+    res.should.be.deep.equal({
+      statusCode: 302,
+      headers: {
+        'Location': 'https://frenchpastries.org'
+      },
+      body: ''
+    })
+  })
+
+  specify('badRequest should be correct bad request response', function() {
+    const res = util.badRequest('Error')
+    res.should.be.deep.equal({
+      statusCode: 400,
+      headers: {},
+      body: 'Error'
+    })
+  })
+
+  specify('forbidden should be correct forbidden response', function() {
+    const res = util.forbidden('Error')
+    res.should.be.deep.equal({
+      statusCode: 403,
+      headers: {},
+      body: 'Error'
+    })
+  })
+
+  specify('internalError should be correct internal server error', function() {
+    const res = util.internalError('Error')
+    res.should.be.deep.equal({
+      statusCode: 500,
+      headers: {},
+      body: 'Error'
+    })
+  })
+
+  specify('contentType to generate a new response with correct contentType', function() {
+    const res = util.response('Ok')
+    const newRes = util.contentType(res, 'application/json')
+    res.should.be.deep.equal({
+      statusCode: 200,
+      headers: {},
+      body: 'Ok'
+    })
+    newRes.should.be.deep.equal({
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: 'Ok'
+    })
+    newRes.should.not.be.equal(res)
+  })
+})
