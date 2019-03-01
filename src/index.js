98c864fe7f925623efe37264ba5d7a2215494e08
Guillaume Hivert <hivert.is.coming@gmail.com>
2019-03-01 15:32:52 +0100

    Improve error page (#5)
    * :heavy_plus_sign: Add Handlebars to simplify the generation of the error page. The error page prints as information as possible in the web page.
    
    * :heavy_plus_sign: Add chalk to improve display of error messages in terminal.
    
    * :sparkles: Detect if the server is in development
    
    * :recycle: Improve response handling
    
    * :recycle: Add error handling in catch promise
    
    * :sparkles: Add handlebars and error page template
    
    * :recycle: Improve response handling
    
    * :sparkles: Add error page on correct response
    
    * :lipstick: Improve title on error page
    
    * :lipstick: Improve error messages in terminal
    
    * :lipstick: Improve error handling
    
    * :bug: Fix undefined body

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const http = require('http')
 const url = require('url')
+const chalk = require('chalk')
 
 const utils = require('./response')
+const errorPage = require('./error-page')
 
 const setURL = request => request.url = url.parse(request.url, true)
+
+const isDev = process.env.NODE_ENV !== 'production'
 
 const extractBody = request => new Promise(resolve => {
   let body = ''
@@ -14,16 +18,48 @@ const extractBody = request => new Promise(resolve => {
   })
 })
 
-const internalErrorMessage = 'Internal Server Error. Please, contact your administrator.'
+const bodyOrErrorPage = content => {
+  if (content.body) {
+    if (content.statusCode) {
+      return content.body
+    } else {
+      if (isDev) {
+        return errorPage({
+          message: `The object you provided is:\n${JSON.stringify(content, null, 2)}`,
+        })
+      } else {
+        return 'Internal Server Error'
+      }
+    }
+  } else {
+    if (content.statusCode) {
+      return undefined
+    } else {
+      if (isDev) {
+        const isError = content instanceof Error
+        return errorPage({
+          message: isError ? content.message : JSON.stringify(content, null, 2),
+          stackTrace: isError ? content.stack : undefined
+        })
+      } else {
+        return 'Internal Server Error'
+      }
+    }
+  }
+}
 
 const normalizeResponse = content => {
-  if (typeof content === 'string') {
-    return utils.internalError(content)
-  } else {
+  if (typeof content === 'object') {
     return {
       statusCode: content.statusCode || 500,
       headers: content.headers || {},
-      body: content.body || (content.statusCode ? undefined : internalErrorMessage)
+      body: bodyOrErrorPage(content)
+    }
+  } else {
+    if (isDev) {
+      return utils.internalError(errorPage({ message: JSON.stringify(content) }))
+    } else {
+      return utils.internalError(JSON.stringify(content))
     }
   }
 }
@@ -34,17 +70,36 @@ const setHeaders = (response, headers) => {
 }
 
 const sendResponse = response => content => {
-  const { statusCode, headers, body } = normalizeResponse(content)
+  const { statusCode, headers, body } = content
   response.statusCode = statusCode
   setHeaders(response, headers)
-  response.write(body)
+  if (body) { response.write(body) }
   response.end()
+}
+
+const normalizeError = error => {
+  if (error instanceof Error) {
+    if (isDev) {
+      console.error(chalk.bold.red(error.stack))
+    }
+    return {
+      statusCode: 500,
+      headers: {},
+      body: errorPage({
+        message: error.message,
+        stackTrace: error.stack,
+      }),
+    }
+  } else {
+    return normalizeResponse(error)
+  }
 }
 
 const handleResponse = (handler, request, response) => {
   Promise.resolve(handler(request))
+    .then(normalizeResponse)
+    .catch(normalizeError)
     .then(sendResponse(response))
-    .catch(sendResponse(response))
 }
 
 const handleRequests = handler => (request, response) => {
