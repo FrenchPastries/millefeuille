9a8aa0fdc6803aac2f59534741868aceafa66e95
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-02-01 19:05:29 +0100

    :recycle: Fix Headers type

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,11 @@
 import * as http from 'http'
 import * as url from 'url'
 
+export type Headers = { [key: string]: any }
 export interface IncomingRequest<Body = any> extends http.IncomingMessage {
   body: Body
   location?: url.URL
   [key: string]: any
-}
-
-export type Headers = {
-  [key: string]: string
 }
 
 export type ServerResponse<Body> = {
